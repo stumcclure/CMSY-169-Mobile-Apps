3a7c6801fa1baabaa34dad498ab23fb3a667e93d
stuwm <stu.w.mcclure@gmail.com>
2022-03-20 13:36:59 -0400

    Initial commit...

---
+++ app/src/main/java/com/example/adroid1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.adroid1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// This is the main class for the Stu MccLure App.
+class MainActivity : AppCompatActivity() {
+
+    // Override the onCreate() Function for the Stu McClure App.
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
